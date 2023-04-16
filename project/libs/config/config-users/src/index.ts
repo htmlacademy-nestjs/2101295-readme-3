9f125690b19d077efb1ef4db7200afa6e776e3e2
Николай <rukin.nik.1996@mail.ru>
2023-04-16 19:03:57 +0700

    created database on mongo

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/config-config-users';
+export * from './lib/config-users.module';
+export { default as dbConfig } from './lib/config/db.config';
+export { default as appConfig } from './lib/config/app.config';
+export * from './lib/get-mongoose-options';
